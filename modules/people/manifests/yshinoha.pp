d946e68cc38cc76491d48e29f46a3db539d62bf6
yshinoha <yshinoha@schoo.jp>
2013-11-15 18:07:41 +0900

    update

--- modules/people/manifests/yshinoha.pp
+++ modules/people/manifests/yshinoha.pp
@@ -8,8 +8,11 @@ class people::yshinoha {
     include screen
 
     package {
+        'Kobito':
+          source   => 'http://kobito.qiita.com/download/Kobito_v1.2.0.zip',
+          provider => 'compressed_app';
         'Vagrant1.3.5':
             provider => 'pkgdmg',
-            source   => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg'
+            source   => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg';
     }
 }
