85fd3ee393065da90c12a1bb9a5da28da56ae614
SHINOHARAYUKI <yshinoha@SHINOHARAYUKI-no-MacBook-Air.local>
2013-11-29 01:17:28 +0900

    update

--- modules/people/manifests/yshinoha.pp
+++ modules/people/manifests/yshinoha.pp
@@ -28,7 +28,10 @@ class people::yshinoha {
           source   => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg';
         'GoogleJapaneseInput':
           source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
-          provider => pkgdmg;
+          provider => 'pkgdmg';
+        'AdobeCreativeCloud':
+          source => "https://ccmdls.adobe.com/AdobeProducts/KCCC/1/osx10/CreativeCloudInstaller.dmg",
+          provider => 'pkgdmg';
     }
 
     # settings for dotfiles
@@ -56,8 +59,10 @@ class people::yshinoha {
     $vim      = "${home}/.vim"
     $neobundle   = "${vim}/bundle"
     $powerline   = "${neobundle}/vim-powerline"
-    $fontpatcher = "${neobundle}/vim-powerline/fontpatcher/fontpatcher"
-    $inconsolata = "${neobundle}/vim-powerline/fontpatcher/Inconsolata.otf"
+    $fontpatcherdir = "${neobundle}/vim-powerline/fontpatcher"
+    $fontpatcher = "${fontpatcherdir}/fontpatcher"
+    #$fontpatcher = "${fontpatcherdir}"
+    $inconsolata = "${fontpatcherdir}/Inconsolata.otf"
     $wgetInconsolata = "wget http://levien.com/type/myfonts/Inconsolata.otf"
     $vimPluginInstall = "yes | vim -c 'q'"
 
@@ -85,13 +90,14 @@ class people::yshinoha {
       creates => "${fontpatcher}",
       require => Repository[$neobundle],
     }
+
     exec { $wgetInconsolata:
-      cwd => $fontpatcher,
+      cwd => $fontpatcherdir,
       creates => "$inconsolata",
       require => Package['wget'],
     }
     exec { "fontforge -script ${fontpatcher} ${inconsolata}":
-      cwd => $fontpatcher,
+      cwd => $fontpatcherdir,
       require => [ Exec[$vimPluginInstall], Exec[$wgetInconsolata], Package['fontforge'] ],
     }
 
